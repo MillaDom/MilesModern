21b80135c41567157577706d6e48d5795333e80b
MillaDom <milladom@gmail.com>
2022-11-17 19:19:03 +0100

    BonusMilesService changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
     public int calculate(int a) {
         int cost;
-        if (a > 20) {
+        if (a >= 20) {
             cost = a / 20;
         } else {
-            cost = a;
+            cost = 0;
         }
         return cost;
     }
